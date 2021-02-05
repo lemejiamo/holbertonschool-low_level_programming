424ba0846ff5d36883ea9972b0c99a5da8b56b70
lemejiamo <luismejia69@gmail.com>
2021-02-05 18:47:01 -0500

    lowercase_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+/*
+ * main - el alfabeto en minuscula
+ * Return: siempre 0
+*/
+
+int main (void)
+{
+        int n;
+        for (n=97; n <= 122; n++)
+                {
+                        putchar (n);
+                }
+	putchar('\n');
+	return (0);
+}
+
