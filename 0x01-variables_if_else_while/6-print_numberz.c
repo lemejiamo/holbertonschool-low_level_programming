bbae3e9722fc2a4ed4c1d4f8b058ebf4bd06f1a7
lemejiamo <luismejia69@gmail.com>
2021-02-05 21:59:30 -0500

    counting_on

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+# include <stdlib.h>
+/**
+ * main -  seguimos contando
+ * Return: 0
+*/
+
+int main(void)
+
+{
+	int n;
+		for (n = 48; n <= 57; n++)
+		{
+			putchar(n);
+		}
+		putchar('\n');
+	return (0);
+}
