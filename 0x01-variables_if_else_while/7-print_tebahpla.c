3424044a429abc494afddc22f1a7a4ba8b112434
lemejiamo <luismejia69@gmail.com>
2021-02-05 22:11:46 -0500

    sever le tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+/**
+ * main - sever le tebahpla
+ * Return: siempre 0
+*/
+
+int main(void)
+{
+int n;
+for (n = 122; n >= 97; n--)
+{
+putchar (n);
+}
+putchar('\n');
+return (0);
+}
