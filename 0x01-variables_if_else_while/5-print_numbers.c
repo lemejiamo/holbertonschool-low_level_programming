8ca867c332d7529f0a2e5623223db0827bf5d191
lemejiamo <luismejia69@gmail.com>
2021-02-05 21:35:26 -0500

    counting_and_acounting

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+/**
+ * main - a contar numeros
+ * Return: siempre 0
+*/
+int main(void)
+
+{
+	int n;
+		for (n = 0; n <= 9; n++)
+		{
+			printf("%d", n);
+		}
+		printf("\n");
+	return (0);
+
+}
