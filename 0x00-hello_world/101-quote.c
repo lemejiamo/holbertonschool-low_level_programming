e1b1df937e34155ba6be4cd01615d6d0f17424ef
= <=>
2021-02-06 17:15:03 -0500

    Learning_c_basics

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -  Anothe funtion to print in the screen a message
+ * Return: 1
+ */
+int main(void)
+{
+		write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	return (1);
+}
+
