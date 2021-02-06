7a7530cb27b97e6504486f2bf42d6c86490e7869
lemejiamo <luismejia69@gmail.com>
2021-02-05 23:42:31 -0500

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+# include <stdio.h>
+# include <stdlib.h>
+/**
+ * main -  seguimos contando
+ * Return: 0
+*/
+
+int main(void)
+{
+	int n;
+		for (n = 48; n <= 57; n++)
+		{
+			putchar(n);
+		{
+		for (n = 97; n <= 102; n++)
+		{
+			putchar(n);
+		}
+	putchar('\n');
+	return (0);
+}
