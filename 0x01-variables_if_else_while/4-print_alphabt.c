d0a9dea2df455e9c3c987a48ce19890ed499b617
lemejiamo <luismejia69@gmail.com>
2021-02-05 20:36:40 -0500

    less_thath_e_q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+# include <stdio.h>
+# include <stdlib.h>
+/*
+ * main - el alfabeto en minuscula
+ * Return: siempre 0
+*/
+
+int main (void)
+
+{
+	int n;
+		for (n=97; n <= 122; n++) 
+			if ( (n != 101) && (n != 113 ) ) 
+				{
+					putchar (n);
+				}
+		putchar('\n');
+	return (0);
+}
+
