687a1c886fd2d8d7423cb0f36ceee9364f818a3e
= <=>
2021-02-09 23:13:09 -0500

    Learning to use bucles

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,15 +1,18 @@
 # include <stdio.h>
 # include "holberton.h"
-
-int main (void)
-
+/**
+ * main - learning arrays and  loops
+ *
+ * Return: 0
+ */
+int main(void)
 {
 	int n;
-	char name[10] = {'H','o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
- 	for (n = 0; n <= 9; n++)
-	     {
-		_putchar(name[n]);
-	     }
+	char name[10] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n' };
+			for (n = 0; n <= 9; n++)
+			{
+				_putchar(name[n]);
+			}
 	_putchar('\n');
 	return (0);
 }
