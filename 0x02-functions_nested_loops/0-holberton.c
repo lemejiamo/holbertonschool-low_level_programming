d3877c4edfa5689b2b682eac92082508855c9f47
= <=>
2021-02-09 23:24:44 -0500

    Learning to use loops

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -8,8 +8,8 @@
 int main(void)
 {
 	int n;
-	char name[10] = "Holberton";
-			for (n = 0; n <= 9; n++)
+	char name[10] =  {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n' };
+			for (n = 2; n <= 9; n++)
 			{
 				_putchar(name[n]);
 			}
