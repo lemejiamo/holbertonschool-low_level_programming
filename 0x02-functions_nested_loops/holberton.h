ae601fa28808eb33898718429f8d983deea5564d
= <=>
2021-02-09 22:59:47 -0500

    HEADER

---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,22 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+/* similar a putchar */
+
+int _putchar(char c);
+
+/* imprime el alfabeto */
+
+void print_alphabet(void);
+
+/* imprime el alfabeto  x 10 */
+
+void print_alphabet_x10(void);
+
+/* is minuscula */
+
+int _islower(int c);
+
+
+
+#endif
