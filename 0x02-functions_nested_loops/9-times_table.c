9b87611ca6ac67a5d13e5a2b7de061291771a7ca
= <=>
2021-02-10 14:16:40 -0500

    Multipliquemos

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+
+/**
+ * times_table -  lest do it ***
+ *
+ */
+
+void times_table(void)
+
+{
+	int table, number;
+
+	for (table = 0 ; table <= 9; table++)
+	{
+		for (number = 0; number <= 9 ; number++)
+		{
+			int times = table * number;
+
+			if (times < 10)
+			{
+				if (number > 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				_putchar('0' + times);
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar('0' + (times / 10));
+				_putchar('0' + (times % 10));
+			}
+			if (number < 9)
+				_putchar(',');
+		}
+		_putchar('\n');
+	}
+}
