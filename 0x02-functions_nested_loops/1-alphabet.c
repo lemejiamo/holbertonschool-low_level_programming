6410061e754ff7e33207b50f70f8a40062202093
= <=>
2021-02-09 23:34:30 -0500

    print_alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * print_alphabet - print the alphabet
+ */
+
+void print_alphabet(void)
+{
+	int n;
+
+	for (n = 97; n <= 122; n++)
+	{
+		_putchar(n);
+
+	}
+	_putchar('\n');
+
+}
