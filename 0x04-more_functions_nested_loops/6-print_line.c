042e095fe02162fa44ca24fe24c690be32474069
= <=>
2021-02-12 12:29:55 -0500

    lets print a line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,23 +2,21 @@
 
 /**
  * print_line - let's do it a way
- *
+ * @n:  entero
  * Return: void
  */
 
 void print_line(int n)
 {
-
 int contador;
 
-	if(n > 0)
-    	{
-        	for(contador = 0; contador <= n; contador++)
-        	{
-        	_putchar('_');
-        	}
-    	}
-    	_putchar('\n');
-return;
+	if (n > 0)
+	{
+		for (contador = 0; contador <= n; contador++)
+		{
+			_putchar ('_');
+		}
+	}
+_putchar ('\n');
 }
 
