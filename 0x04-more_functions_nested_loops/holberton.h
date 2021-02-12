16da8ad78bfa5b294756c099e17ab572bd0aa166
= <=>
2021-02-12 11:03:32 -0500

    0x04-more_funtions_nested_loops

---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,16 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/* similar a putchar */
+
+int _putchar(char c);
+
+/* 6 - lets print a line */
+
+void print_line(int n);
+
+/* 7. I feel like I am diagonally parked in a parallel universe  */
+
+void print_diagonal(int n);
+
+#endif
