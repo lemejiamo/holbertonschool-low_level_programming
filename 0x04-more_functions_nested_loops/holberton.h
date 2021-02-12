be863ad2f49c40887e525e5bd831ae561c2a187b
= <=>
2021-02-12 18:34:32 -0500

    HEADER FILE

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -5,7 +5,31 @@
 
 int _putchar(char c);
 
+/*  function that checks for uppercase character. */
+
+int _isupper(int c);
+
+/*  function that checks for a digit (0 through 9). */
+
+int _isdigit(int c);
+
+/*  function that multiplies two integers. */
+
+int mul(int a, int b);
+
+/* function that prints the numbers, from 0 to 9, followed by a new line. */
+
+void print_numbers(void);
+
+/* function that prints the numbers, from 0 to 9, followed by a new line. */
+
+void print_most_numbers(void);
+
+/* function that prints 10 times the numbers, from 0 to 14, followed by a new line. */
+
+void more_numbers(void);
 /* 6 - lets print a line */
+
 
 void print_line(int n);
 
@@ -16,6 +40,17 @@ void print_diagonal(int n);
 /* You are so much sunshine in every square inch  */
 
 void print_square(int size);
+
+/* Numbers have life; they're not just symbols on paper */
+
+void print_number(int n);
+
+
+/*  function that prints a triangle, followed by a new line  */
+
+void print_triangle(int size);
+
+
 
 #endif
 
