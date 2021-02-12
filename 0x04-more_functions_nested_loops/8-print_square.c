7eabd9a20d646aef349e4be56f58150e8a1b8b32
= <=>
2021-02-12 14:10:47 -0500

    print a square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * print_square - make it a square
+ * @size: the size of a square
+ *
+ */
+
+void print_square(int size)
+
+{
+
+if (size > 0)
+{
+int fila, columna ;
+
+for (fila = 0 ; fila < size; fila++)
+	{
+	for ( columna = 0; columna < size; columna++) 
+		{
+			_putchar (35);
+		}
+	_putchar('\n');
+	}
+}
+else
+_putchar('\n');
+
+}
+
