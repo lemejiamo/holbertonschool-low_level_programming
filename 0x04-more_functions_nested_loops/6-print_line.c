410c2985e94a0bbfb3694fa98aac4f7de9949006
= <=>
2021-02-12 10:56:26 -0500

    let's print a way

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/*
+* print_line - let's do it a way
+*
+* Return: void
+*/
+
+void print_line(int n)
+{
+
+int contador;
+
+	if(n > 0)
+    	{
+        	for(contador = 0; contador <= n; contador++)
+        	{
+        	_putchar('_');
+        	}
+    	}
+    	_putchar('\n');
+return;
+}
+
