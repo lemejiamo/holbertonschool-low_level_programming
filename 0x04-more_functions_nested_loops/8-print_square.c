a8ef9371f0d3ca8c7fa867ef29cd5e60ac848f02
= <=>
2021-02-12 14:14:16 -0500

    print a square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,14 +12,14 @@ void print_square(int size)
 
 if (size > 0)
 {
-int fila, columna ;
+int fila, columna;
 
 for (fila = 0 ; fila < size; fila++)
 	{
-	for ( columna = 0; columna < size; columna++) 
-		{
-			_putchar (35);
-		}
+	for (columna = 0; columna < size; columna++)
+	{
+		_putchar (35);
+	}
 	_putchar('\n');
 	}
 }
