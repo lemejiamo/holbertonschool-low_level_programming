2b4c1493a197822d5636bcdbe92d33ebe81ef1af
= <=>
2021-02-12 21:12:32 -0500

    HEADER FILE

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -41,4 +41,14 @@ void jack_bauer(void);
 
 void times_table(void);
 
+/* function that adds two integers and returns the result */
+
+int add(int, int);
+
+/* function that prints all natural numbers from n to 98, followed by a new line */
+
+void print_to_98(int n);
+
+
+
 #endif
