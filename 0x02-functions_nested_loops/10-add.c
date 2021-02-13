bb3343134a52e1470feb8e382533dc531cc91f7e
= <=>
2021-02-12 21:09:16 -0500

    function that adds two integers and returns the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * add - add to integers
+ * @a: integer
+ * @b: integer
+ * Return: resultado
+ */
+
+int add(int a, int b)
+{
+int  r;
+
+r = a + b;
+
+return (r);
+}
