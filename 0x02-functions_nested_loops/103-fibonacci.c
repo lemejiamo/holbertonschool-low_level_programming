c2e40c9077c914e62ea4d64c7bbfedfe59011118
= <=>
2021-02-14 23:29:11 -0500

    finds and prints the sum of the even-valued terms do not exceed 4000000

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - print fibonacci sequence
+ * Return: no way
+ */
+
+int main(void)
+{
+long int number1, number2, resultado, contador, suma;
+number1 = 0;
+number2 = 1;
+contador = 0;
+suma = 0;
+do {
+	resultado = number1 + number2;
+	contador++;
+	number1 = number2;
+	number2 = resultado;
+	if ((resultado % 2) == 0)
+	{
+		suma = suma + resultado;
+	}
+} while (contador < 38);
+printf("%ld", suma);
+putchar ('\n');
+return (0);
+}
