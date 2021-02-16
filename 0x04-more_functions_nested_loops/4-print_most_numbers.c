7757fb8490f49860571c65f4819c1dff0c90c528
= <=>
2021-02-16 15:26:35 -0500

    print_most_numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * print_most_numbers - from 0 to 9
+ * Return: None
+ */
+void print_most_numbers(void)
+{
+int n;
+
+	for (n = 0; n < 10; n++)
+		{
+			if (n != 2 && n != 4)
+			{
+			_putchar (n + '0');
+			}
+		}
+_putchar('\n');
+}
