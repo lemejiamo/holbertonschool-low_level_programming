989ff8517c33a6b7f2148a7d3109670b2f01e945
= <=>
2021-02-16 16:41:00 -0500

    swap values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * swap_int - swap two integers
+ * @a: integar
+ * @b: integer
+ * Return: none 
+ */
+void swap_int(int *a, int *b)
+{
+int i;
+	i = *a;	
+	*a = *b;
+	*b = i;
+}
+
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * swap_int - swap values
+ * @a: value one
+ * @b: value two
+ * Return: none
+ */
+
+void swap_int(int *a, int *b)
+{
+
+int i;
+
+i = *a
+*a = *b
+*b = i
+
+}
