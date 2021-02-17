aa86dedd1ce72c349cff05569fef52045e55fa89
= <=>
2021-02-17 12:04:29 -0500

    code check

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,29 +1,31 @@
 #include "holberton.h"
-
-
 /**
  * puts_half - puts_half2
  * @str: string
  * Return: none
  */
-
 void puts_half(char *str)
 {
-int n, mitad; n = 0; while (str[n] != '\0') // saco el largo de la cadena
-{
-n++;
-}
-if ( n % 2 == 0) // establecer el punto medio para iniciar la impresion
-{
-            mitad = n /2; // si es par
-        else // si es impar
-            {
-            mitad = ( n - 1 ) / 2;
-            mitad++;
-            }
-for (mitad ; mitad <= n; mitad++) // desde la amitad empiezo a imprimir la cadena
-        {
-        putchar(str[mitad]);
-        }
-putchar('\n');
+int n, mitad;
+
+n = 0;
+
+	while (str[n] != '\0')
+		{
+		n++;
+		}
+	if ( n % 2 == 0)
+		{
+		mitad = n /2;
+		{
+	else
+		{
+		mitad = ( n - 1 ) / 2;
+		mitad++;
+		}
+for (mitad ; mitad <= n; mitad++)
+	{
+	_putchar (str[mitad]);
+	}
+_putchar ('\n');
 }
