a474ad665a96000231c199e8636861b58b8a47cd
= <=>
2021-02-17 00:12:50 -0500

    puts_2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * puts2 - puts 2
+ * @str: string
+ * Return: none
+ */
+void puts2(char *str)
+{
+
+while (*str != '\0')
+	{
+	if(*str % 2 == 0)
+		{
+		_putchar(*str);
+		}
+	str++;
+	}
+_putchar ('\n');
+}
