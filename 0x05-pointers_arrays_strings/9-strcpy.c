cdf348f46abcad5ccf49c5d30387d6068304a781
= <=>
2021-02-18 00:05:35 -0500

    create a copy a string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * *_strcpy - copy string
+ * @dest: destiny
+ * @src: source
+ * Return: char
+ */
+char *_strcpy(char *dest, char *src)
+{
+
+int i, t;
+i = 0;
+while (src[i] != '\0')
+        {
+        i++;
+        }
+t = ++i;
+
+for ( i = 0; i <= t; i++)
+        {
+        dest[i] = src[i];
+        }
+return (dest);
+}
