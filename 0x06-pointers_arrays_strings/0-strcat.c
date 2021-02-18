d52c4d4bce569219a0160fe3413b168cee9283c5
= <=>
2021-02-18 12:58:23 -0500

    concatenate strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,13 +10,14 @@
 char  *_strcat(char *dest, char *src)
 
 {
-int i = 0, j = 0 ; /* couters to arrays */ 
+int i = 0, j = 0 ; /* couters to arrays */
 
 while (dest[i] != '\0')  /* cicle to find the position to begin to write */
 	i++;
 
-for (; (dest[i + j] = *src++) != '\0';)
+for (; (dest[i] = src[j]) != '\0';)
 	{
+	i++;
 	j++;
 	}
 
