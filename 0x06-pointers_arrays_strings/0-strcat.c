ca8e91783456e520a8ffbe83c45a25dafbf2b4e3
= <=>
2021-02-18 12:54:49 -0500

    concatenate strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * *_strcat - concatenates two strings
+ * @dest: destiny
+ * @src: source
+ * Return: char
+ */
+
+char  *_strcat(char *dest, char *src)
+
+{
+int i = 0, j = 0 ; /* couters to arrays */ 
+
+while (dest[i] != '\0')  /* cicle to find the position to begin to write */
+	i++;
+
+for (; (dest[i + j] = *src++) != '\0';)
+	{
+	j++;
+	}
+
+
+
+
+return (dest);
+
+}
