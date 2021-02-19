02748e8c51dc09168ee0970d894107250ab6d294
= <=>
2021-02-19 08:17:16 -0500

    Cap_string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * *cap_string - strin to cap
+ * @string: string to cap
+ * Return: char
+ */
+
+char *cap_string(char *string)
+{
+int i = 0;
+
+while (string[i] != '\0')
+{
+	if (string[i] == '\t' || string[i] == '\n' || string[i] == ' '
+	    || string[i] == ',' || string[i] == ';' || string[i] == '.'
+	    || string[i] == '!' || string[i] == '?' || string[i] == '"'
+	    || string[i] == '(' || string[i] == ')' || string[i] == '{'
+	    || string[i] == '}')
+	{
+		if (string[i + 1] >= 'a' && string[i + 1] <= 'z')
+		{
+			string[i + 1] = (string[i + 1] - 32);
+		}
+	}
+	i++;
+}
+return (string);
+}
