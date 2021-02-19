bd0ad71921aec135273009b734536f25d823c89e
= <=>
2021-02-19 09:44:09 -0500

    Cap_string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,9 +11,18 @@ int i = 0;
 
 while (string[i] != '\0')
 {
+	if (string[i] == 0)
+		{
+			if (string[i + 1] >= 'a' && string[i + 1] <= 'z')
+			 {
+                        string[i + 1] = (string[i + 1] - 32);
+			 }
+
+		}
+
 	if (string[i] == '\t' || string[i] == '\n' || string[i] == ' '
 	    || string[i] == ',' || string[i] == ';' || string[i] == '.'
-	    || string[i] == '!' || string[i] == '?' || string[i] == '"'
+	    || string[i] == '!' || string[i] == '?' || string[i] == '\"'
 	    || string[i] == '(' || string[i] == ')' || string[i] == '{'
 	    || string[i] == '}')
 	{
