ded8a8985121d8086d88967172303dbc100a4af3
= <=>
2021-02-22 12:55:02 -0500

    HEADER FILE

---
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -0,0 +1,42 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+0. memset 
+
+char *_memset(char *s, char b, unsigned int n);
+
+1. memcpy 
+
+char *_memcpy(char *dest, char *src, unsigned int n);
+
+2. strchr
+
+char *_strchr(char *s, char c);
+
+3. strspn
+
+unsigned int _strspn(char *s, char *accept);
+
+4. strpbrk
+
+char *_strpbrk(char *s, char *accept);
+
+5. strstr
+
+char *_strstr(char *haystack, char *needle);
+
+
+6. Chess is mental torture
+
+void print_chessboard(char (*a)[8]);
+
+7. The line of life is a ragged diagonal between duty and desire
+
+void print_diagsums(int *a, int size);
+
+8. Double pointer, double fun 
+
+void set_string(char **s, char *to);
+
+
+#endif
