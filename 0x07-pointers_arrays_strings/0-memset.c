105c4b37f71c8742f7a759928cbcd17aca456c61
= <=>
2021-02-22 13:09:56 -0500

    memory set

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,10 +1,11 @@
 #include "holberton.h"
 
 /**
- * _memset - fills the memory with a constant char
+ * *_memset - fills the memory with a constant char
  * @n: unsigned int
  * @b: a char to fill the array
  * @s: our arr
+ * Return:  s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
