cae5a3af5b3f0dfefcc6a0141dcf32e5c8abc2c4
= <=>
2021-02-22 20:02:57 -0500

    searching coincidence

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,27 +1,32 @@
 #include "holberton.h"
 
 /**
- *
- *
+ * _strspn - search for coincidences
+ * @s: string to read
+ * @accept: chars to search
+ * Return: the count
  */
 
 unsigned int _strspn(char *s, char *accept)
 {
 int i, j, c;
-j=c=0;
-for (i = 0; s[i] != '\0'; i++) {
-if (accept[j]== '\0')
+j = c = 0;
+for (i = 0; s[i] != '\0'; i++)
 	{
-	return (c);
+
+	if (accept[j] == '\0')
+		{
+		return (c);
+		}
+
+	for (j = 0; accept[j] != '\0'; j++)
+		{
+		if (accept[j] == s[i])
+			{
+			c += 1;
+			break;
+			}
+		}
 	}
-for (j=0; accept[j] != '\0'; j++)
-	{
- 	if (accept[j] == s[i])
-        {
-        c += 1;
-        break;
-        }
-    }
-}
 return (c);
 }
