f35073dc3abb0ad5a63426e4e6987e775755b12e
= <=>
2021-02-25 11:53:09 -0500

    checks if a number is prime

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+
+/**
+ * check -  checks if a number is  prime
+ * @m: divisor
+ * @n: number to check
+ * Return: a integer
+ */
+
+
+int check(int m, int n)
+{
+if ((n % m) != 0)
+	{
+	if (m > (n / 2))
+		{
+		return (1);
+		}
+	return (check(m + 2, n));
+	}
+return (0);
+}
+
+
+/**
+ * is_prime_number - checks if a number is prime
+ * @n: intger to evaluate
+ * Return: 1 is prime or 0 is not
+ */
+int is_prime_number(int n)
+
+{
+
+if (n <= 0)
+	return (0);
+
+if ((n % 2) == 0)
+	return (0);
+
+return (check(3, n));
+
+}
+
