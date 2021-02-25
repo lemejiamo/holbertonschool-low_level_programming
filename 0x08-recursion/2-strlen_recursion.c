2f3014ae2b9d36c6c999fac593806bdaa1e148b2
= <=>
2021-02-24 20:17:17 -0500

    string length with recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * _strlen_recursion - strength of a string
+ * @s: string to read
+ * Return: integer
+ */
+int _strlen_recursion(char *s)
+{
+int suma;
+
+if (*s == '\0')
+	return (0);
+
+return (1 + _strlen_recursion(s + 1));
+
+}
