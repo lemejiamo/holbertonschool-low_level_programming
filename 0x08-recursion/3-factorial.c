e11e55fee9cf41ca94f3e9d4d98e847de2348d10
= <=>
2021-02-24 20:32:53 -0500

    calculate the factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * factorial - calculate factorial
+ * @n: integer
+ * Return: integer
+ */
+int factorial(int n)
+{
+
+if (n == 0)
+	return (1);
+
+return (n * factorial(n - 1));
+
+}
