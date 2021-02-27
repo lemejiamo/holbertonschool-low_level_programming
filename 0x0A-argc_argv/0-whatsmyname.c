eee7f65f1809c85c33f387b52748e0d8cf2be590
= <=>
2021-02-27 15:37:15 -0500

    WHAT IS MY NAME

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,19 +1,18 @@
-#include "holberton.h"
 #include <stdio.h>
 /**
- *
- *
- *
+ * main - main
+ * @argc: integer
+ * @argv: array
+ * Return: always 0
  */
 
 int main(int argc, char *argv[])
 
 {
-
 int i;
 
 for (i = 0; i < argc; i++)
-	printf ("%s\n", argv[i]);
+	printf("%s\n", argv[i]);
 
-return 0;
+return (0);
 }
