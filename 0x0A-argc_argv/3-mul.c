c1d48f9071ef8f5aa16b72e6e57d4731fd62e9c1
= <=>
2021-02-27 19:21:30 -0500

    multiplication

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - the funtion
+ * @argc: integer
+ * @argv: string
+ * Return: a integer
+ */
+int main(int argc, char *argv[])
+{
+int resultado;
+
+if (argc > 3)
+	{
+	printf("Error\n");
+	return (1);
+	}
+
+else
+	{
+	resultado = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", resultado);
+	return (0);
+	}
+}
