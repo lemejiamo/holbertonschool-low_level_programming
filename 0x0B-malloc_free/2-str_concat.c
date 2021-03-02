ef31789552310deb695bb54c4746057c03696c43
= <=>
2021-03-02 17:33:04 -0500

    concatenate two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,18 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * *str:concat - concatenate two strings
+ * *str_concat - concatenate two strings
  * @s1: string one
  * @s2: string two
  * Return: Pointer or NULL
  */
 char *str_concat(char *s1, char *s2)
 {
-/* declaracion de las variable */
 int i, j, k, l = 0;
-char *concatenado;
-/* caso especial si alguna de las cadenas es igual a NULL */
-/* 1er paso leer el tamaño de las cadenas */
+char *conc;
+
 if (*s1 == '\0')
 	s1 = "";
 if (*s2 == '\0')
@@ -24,16 +22,18 @@ for (i = 0; s1[i] != '\0'; i++)
 
 for (j = 0; s2[j] != '\0'; j++)
 {}
-/* 2do paso asignar el tamaño al concatenado */
-	concatenado = malloc(sizeof(char) * (i + j + 1));
-/* 3er paso escribir las cadenadas en el nuevo array */
+
+conc = malloc(sizeof(char) * (i + j + 1));
+if (conc == NULL)
+	return (NULL);
+
 for (k = 0; k < i; k++)
 	{
-	concatenado[k] = s1[k];
+	conc[k] = s1[k];
 	}
 for (; k <= (i + j); k++, l++)
 	{
-	concatenado[k] = s2[l];
+	conc[k] = s2[l];
 	}
-return (concatenado);
+return (conc);
 }
