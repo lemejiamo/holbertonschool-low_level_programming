e504ed484aa7540ef823123f66650e94e774acec
= <=>
2021-03-02 14:11:57 -0500

    create a array fill with a specified char

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,14 +12,15 @@ char *create_array(unsigned int size, char c)
 
 unsigned int i = 0;
 char  *array;
-array = malloc(sizeof(int) * size);
+array = malloc(sizeof(char) * size);
 
 if (array == NULL)
 	{
 	return ('\0');
 	}
-if (!size)
+if (!size) /* size == 0 */
 return ('\0');
+
 	for (; i < size; i++)
 		{
 		array[i] = c;
