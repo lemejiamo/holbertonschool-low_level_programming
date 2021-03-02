7a00859fde4f4444f78d2fda2b6657c56bdd73fa
= <=>
2021-03-02 17:23:10 -0500

    concatenate two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,17 +18,25 @@ char *concatenado;
 
 /* caso especial si alguna de las cadenas es igual a NULL */
 
-/* if (*s1 == '\0' || *s2 == '\0') */
-/*	return (NULL); */
 
 /* 1er paso leer el tamaño de las cadenas */
 
-for (i = 0; s1[i] != '\0'; i++)
-	{}
+if (*s1 == '\0')
+	s1 = "";
 
-for (j = 0; s2[j] != '\0'; j++)
+else
+	{
+	for (i = 0; s1[i] != '\0'; i++)
 	{}
+	}
 
+if (*s2 == '\0')
+        s2 = ""; 
+else
+	{
+	for (j = 0; s2[j] != '\0'; j++)
+	{}
+	}
 /* 2do paso asignar el tamaño al concatenado */
 
 concatenado = malloc(sizeof(char) * (i + j + 1));
@@ -36,7 +44,7 @@ concatenado = malloc(sizeof(char) * (i + j + 1));
 
 /* 3er paso escribir las cadenadas en el nuevo array */
 
-for (k = 0; k <= i; k++)
+for (k = 0; k < i; k++)
 	{
 	concatenado[k] = s1[k];
 	}
@@ -46,7 +54,6 @@ for (; k <= (i +j); k++, l++)
 	concatenado[k] = s2[l];
 	}
 
-concatenado[k + 1] = '\0';
 return (concatenado);
 
 }
