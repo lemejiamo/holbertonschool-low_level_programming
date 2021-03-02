1ed16f5f43d24e23db4327d8639a44b3ef112f4c
= <=>
2021-03-02 14:55:16 -0500

    prueba 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *
+ *
+ *
+ *
+ */
+char *_strdup(char *str)
+
+
+{
+
+char *dup; /*declaramos el puntero que va ser la copia */
+int i = 0;
+int j = 0; /* iniciamos la variable que nos sirve de indice para hacer la copia */
+
+/*1er paso contar la cadena dada */
+/* ciclo para contar el largo de la cadena */
+while (str[i] != '\0')
+	{
+	i++;
+	}
+
+/* tenemos un valor en i */
+
+				/* 1er caso cuaanod la cadena esta vacia */
+if (!str) 			/* str == 0 */
+	return ('\0');
+
+/* 2do caso cadena contiene un valor */
+
+dup = malloc(sizeof(char) * i); 	/* asignamos el tañomaño con mallox */
+
+for ( ; j <= i; j++)
+	{
+	dup[j] = str[j];
+	}
+return (dup);
+}
