8a064d064f32096cbaa350265045f4a39225bd9d
= <=>
2021-03-02 14:59:25 -0500

    prueba 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -16,17 +16,16 @@ char *dup; /*declaramos el puntero que va ser la copia */
 int i = 0;
 int j = 0; /* iniciamos la variable que nos sirve de indice para hacer la copia */
 
-/*1er paso contar la cadena dada */
-/* ciclo para contar el largo de la cadena */
+		/*1er paso contar la cadena dada */
+		/* ciclo para contar el largo de la cadena */
 while (str[i] != '\0')
 	{
 	i++;
 	}
 
-/* tenemos un valor en i */
 
 				/* 1er caso cuaanod la cadena esta vacia */
-if (!str) 			/* str == 0 */
+if (!*str) 			/* str == 0 */
 	return ('\0');
 
 /* 2do caso cadena contiene un valor */
