355a31ac3c5a3e4c0c1b0c00ae87f0c4a4b9cee7
= <=>
2021-03-02 15:29:08 -0500

    Duplicate a string

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,48 +2,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *
- *
- *
- *
+ * *_strdup -  duplicate a string
+ * @str: string
+ * Return: pointer to dup or null
  */
 char *_strdup(char *str)
-
-
 {
-
-char *dup; /*declaramos el puntero que va ser la copia */
+char *dup;
 int i = 0;
-int j = 0; /* iniciamos la variable que nos sirve de indice para hacer la copia */
+int j = 0;
 
-		/*1er paso contar la cadena dada */
-		/* ciclo para contar el largo de la cadena */
-while (str[i] != '\0')
+if (!str)
+	return (NULL);
+
+while (str[i] != NULL)
 	{
 	i++;
 	}
 
+dup = malloc(sizeof(char) * (i + 1));
 
-				/* 1er caso cuaanod la cadena esta vacia */
-if (!str) 			/* str == 0 */
-	return ('\0');
-
-/* 2do caso cadena contiene un valor */
-
-dup = malloc(sizeof(char) * (i + 1)); 	/* asignamos el tañomaño con mallox */
-
-if ( dup == 0)
-	return ('\0');
-
-
+if (dup == NULL)
+	return (NULL);
 
 for ( ; j <= i; j++)
 	{
 	dup[j] = str[j];
 	}
 return (dup);
-
-
-
 
 }
