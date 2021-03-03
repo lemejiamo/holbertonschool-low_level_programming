ed31d03ada64ada8ab65472cdca21aede7645620
= <=>
2021-03-02 22:59:48 -0500

    grid with malloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,13 +16,13 @@ int i, j;
 if (width <= 0 || height <= 0)
 	return (NULL);
 
-array = (int **)malloc(width * sizeof(int *));
+array = malloc(width * sizeof(int *));
 if (array == NULL)
 	return (NULL);
 
 for (i = 0; i < width; i++)
 	{
-	array[i] =(int *)malloc(height * sizeof(int *));
+	array[i] = malloc(height * sizeof(int *));
 	if (array[i] == NULL)
 		{
 		for ( ; i >= 0 ; i--)
