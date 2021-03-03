8ae5afb1e60108915248ffdfb2fe280f0b12163c
= <=>
2021-03-02 21:58:26 -0500

    grid with malloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -2,15 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
-*
-*
-*
-*
+* alloc_grid - fill a grid using malloc
+* @width: width
+* @height: height
+* Return: pointer or null
 */
 
 int **alloc_grid(int width, int height)
 {
-
 int **array;
 int i, j;
 
@@ -18,23 +17,26 @@ if (width <= 0 || height <= 0)
 	return (NULL);
 
 array = malloc(width * sizeof(int *));
-
-if(array == NULL)
+if (array == '\0')
 	return (NULL);
 
 for (i = 0; i < width; i++)
 	{
 	array[i] = malloc(height * sizeof(int));
-	if(array[i] == NULL)
-        	return (NULL);	
+
+	if (array[i] == '\0')
+		{
+		for (j = i; j >= 0 ; j--)
+			free(array[j]);
+			return (NULL);
+		}
 	}
 
-/* 2do paso rellenamor arreglo */
+for (i = 0; i < width; i++)
+	{
+	for (j = 0; j < height; j++)
+		array[i][j] = 0;
+	}
 
-for(i = 0; i < width; i++)
-		{
-		for(j = 0; j < height; j++)
-			array[i][j] = 0;
-		}
-return(array);
+return (array);
 }
