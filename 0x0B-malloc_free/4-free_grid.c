5e0f9d7748600cb9a4a9c873791a689e1f93e19b
= <=>
2021-03-03 20:55:12 -0500

    freedom

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * free_grid - free grid
+ * @grid:  pointer
+ * @height: integer
+ * Return: none
+ */
+void free_grid(int **grid, int height)
+{
+int i = 0;
+
+ while (i < height)
+        {
+            free(grid[i]);
+            i++;
+        }
+        free(grid);
+}
