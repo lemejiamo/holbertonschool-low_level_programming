bcb8fa52e40167fa2d8210efae0d3177e96e0623
= <=>
2021-03-03 21:58:50 -0500

    write a grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,30 +12,30 @@ int **alloc_grid(int width, int height)
 int **array, i;
 
 if (height <= 0 || width <= 0)
-	{
+{
 	return (NULL);
-	}
+}
 
 array = malloc(height * sizeof(int *));
 
 if (array == NULL)
 	{
-		free(array);
-		return (NULL);
+	free(array);
+	return (NULL);
 	}
 
 for (i = 0; i < height; i++)
-	{
-		array[i] = malloc(width * sizeof(int *));
-		if (array[i] == NULL)
+{
+	array[i] = malloc(width * sizeof(int *));
+	if (array[i] == NULL)
+		{
+		for ( ; i >= 0 ; i--)
 			{
-				for ( ; i >= 0 ; i--)
-					{
-						free(array[i]);
-					}
-			free(array);
-			return (NULL);
+			free(array[i]);
 			}
-	}
+		free(array);
+		return (NULL);
+		}
+}
 return (array);
 }
