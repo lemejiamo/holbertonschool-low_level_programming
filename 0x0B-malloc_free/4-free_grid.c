8df8ef98740eca1e4809ce12ee61bb49af39b1e7
= <=>
2021-03-03 21:13:16 -0500

    freedom

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -8,12 +8,11 @@
  */
 void free_grid(int **grid, int height)
 {
-int i = 0;
+int i;
 
-	for (; i < height; i++)
+	for (i = 0; i < height; i++)
 	{
-		free(grid[i]);
+		free(*(grid + i));
 	}
-free(grid[i]);
 free(grid);
 }
