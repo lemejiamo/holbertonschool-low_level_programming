daff15480cc99ffa480d97eb5aaa1f807fdfbb8e
= <=>
2021-03-04 00:10:38 -0500

    argstostr

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -1,45 +1,48 @@
+
 #include <stdlib.h>
 #include <stdio.h>
 /**
-*
+* argstostr -  argstostr
+* @ac: integer
+* @av: string
+* Return: michar
 */
 char *argstostr(int ac, char **av)
 {
-char **michar;
-int i, j;
+char *michar;
+int i, j, m = 0, leng = 0;
 
 if (ac == 0 || **av == '\0')
-        return (NULL);
+	return (NULL);
 
-michar = malloc(sizeof(char) * ac);
-        if (michar == NULL)
-                return (NULL);
 
 for (i = 0; i < ac; i++)
-        {
-        for (j = 0; av[i][j] != '\0'; j++ )
-                {
-                }
-                /* printf("%s\n", *(av + i)); */
-                michar[i] = malloc(sizeof(char) * (j));
-                 if (michar[i] == NULL)
-                      {
-                      return (NULL);
-                      }
-                /* printf("%d\n", j); */
-        }
+{
+	for (j = 0; av[i][j] != '\0'; j++)
+		{
+		leng++;
+		}
 
-for (i = 0; i < ac; i++)
-  {
-        for ( j = 0; av[i][j] != '\0'; j++)
-        {
-            michar[i][j] = av [i][j];
-            /* printf("%c\n", av [i][j]); */
-        }
-        /*michar[i][j] = '\n'; */
-       /* printf("%s\n", michar[i]); */
-  }
- /* j++; */
-/*michar[i][j] = '\0';*/
-return (*michar);
+}
+printf("%d\n", leng);
+
+michar = malloc(sizeof(char) * (leng + ac + 1));
+if (michar == '\0')
+	return (NULL);
+
+i = 0;
+j = 0;
+for (; i < ac; i++)
+{
+	for (j = 0; av[i][j] != '\0'; j++)
+	{
+		michar[m] = av[i][j];
+		m++;
+	}
+michar[m] = '\n';
+m++;
+}
+m++;
+michar[m] = '\0';
+return (michar);
 }
