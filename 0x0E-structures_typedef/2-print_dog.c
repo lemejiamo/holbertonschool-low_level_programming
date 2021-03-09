78c7ffee34967c0b5953b4d0abd171f7a89f7845
= <=>
2021-03-08 21:09:09 -0500

    print info

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,9 +9,32 @@ void print_dog(struct dog *d)
 {
 if(d != '\0')
 {
-	printf("Name: %s\n", (*d).name);
-	printf("Age: %f\n", (*d).age);
-	printf("Owner: %s\n",(*d).owner);
+	if((*d).name != '\0')
+	{
+		printf("Name: %s\n", (*d).name);
+	}
+	else
+	{
+		printf("nil\n");
+	}
+
+	if((*d).age != '\0')
+	{
+		printf("Age: %f\n", (*d).age);
+	}
+	else
+	{
+		printf("nil\n");
+	}
+	if((*d).owner != '\0')
+	{
+		printf("Owner: %s\n",(*d).owner);
+	}
+	else
+	{
+		printf("nil\n");
+	}
+
 }
 }
 
