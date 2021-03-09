22310d0be42c1a24e4fbab9ba16d3368c1f7b79c
= <=>
2021-03-08 19:42:43 -0500

    HEADER FILE

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog {
+/**
+* struct dog - name of structure
+* @name: Dogy name
+* @age: age of a dog
+* @owner: really this need a explanation??
+*/
+struct dog
+{
 
 	char *name;
 	float age;
