d743b49791c9a11081d9b46265e28c65c299aa5e
= <=>
2021-03-09 02:17:08 -0500

    My new Doggy

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,38 @@
 #include <stdio.h>
 #include "dog.h"
 #include <stdlib.h>
+
+
+/**
+ * *_strdup -  duplicate a string
+ * @str: string
+ * Return: pointer to dup or null
+ */
+char *_strdup(char *str)
+{
+char *dup;
+int i = 0;
+int j = 0;
+
+if (!str)
+        return (NULL);
+
+while (str[i] != '\0')
+        {
+        i++;
+        }
+
+dup = malloc(sizeof(char) * (i + 1));
+
+if (dup == NULL)
+        return (NULL);
+
+for ( ; j <= i; j++)
+        {
+        dup[j] = str[j];
+        }
+return (dup);
+}
 /**
  * new_dog - input data
  * @name:name of a dog
@@ -8,18 +40,31 @@
  * @owner:owner of a dog
  * Return: pointer to a dog
  */
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
 
 dog_t *dog;
+
 dog = malloc(sizeof(dog_t));
 if (dog == NULL)
 	return (NULL);
 
-(*dog).name = name;
-(*dog).age =  age;
-(*dog).owner =  owner;
+(*dog).name = _strdup(name);
+if((*dog).name == NULL)
+	{
+		free((*dog).name);
+		return (NULL);
+	}
 
+(*dog).age =  age;
+
+(*dog).owner = _strdup(owner);
+if((*dog).owner == NULL)
+	{
+		free((*dog).owner);
+		return (NULL);
+	}
 return (dog);
 }
 
