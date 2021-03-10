7c754a350fa89e78f93ca9f5abc520f23c2868c4
= <=>
2021-03-10 13:59:17 -0500

    Print a name with a pointer to funtions :)

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,15 @@
 #include <stlib.h>
+#include <stdio.h>
+#include "funtion_pointers.h"
 /**
- * print_name - print name
+ * print_name - prints name
  * @name: type char
  * @f: pointer to a funtion
  * Return: none
  */
 void print_name(char *name, void (*f)(char *))
 {
-if (name != NULL && f != NULL)
-	(*f)(name);
+	if (name != NULL && f != NULL)
+		(*f)(name);
 }
+
