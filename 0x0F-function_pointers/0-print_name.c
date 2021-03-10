6dcfe4d3c19b0659258e8cd03b9827e4ef1ea77a
= <=>
2021-03-10 12:03:51 -0500

    Print a name with a pointer to funtions :)

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * print_name - print name
+ * @name: type char
+ * @f: pointer to a funtion
+ * Return: none
+ */
+void print_name(char *name, void (*f)(char *))
+{
+(*f)(name);
+}
