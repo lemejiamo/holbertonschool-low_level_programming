d045183ffe7ba662f7c0324a74f1343c650be1b9
= <=>
2021-03-11 18:58:25 -0500

    Sum_them_all_args

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,12 +8,14 @@
 int sum_them_all(const unsigned int n, ...)
 {
 unsigned int suma = 0, i = 0;
+va_list ap;
+
+
+va_start(ap, n);
 
 	if (n == 0)
 		return (0);
-va_list ap;
 
-va_start(ap, n);
 	for (; i < n; i++)
 	{
 		suma += va_arg(ap, unsigned int);
