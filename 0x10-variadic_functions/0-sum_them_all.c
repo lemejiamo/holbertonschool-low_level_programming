e51d55ec33cc6f0914803426e529b3a38ef1dab9
= <=>
2021-03-11 18:53:27 -0500

    Sum_them_all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -2,26 +2,24 @@
 #include <stdarg.h>
 /**
  * sum_them_all - sum all the arguments in a  funtion
- *
- *
- *
- *
+ * @n: arguments
+ * Return: sum
  */
 int sum_them_all(const unsigned int n, ...)
 {
 unsigned int suma = 0, i = 0;
 
 	if (n == 0)
-		return 0;
+		return (0);
 va_list ap;
 
 va_start(ap, n);
-	for (; i < n; i++ )
+	for (; i < n; i++)
 	{
 		suma += va_arg(ap, unsigned int);
 	}
 va_end(ap);
 
-return suma;
+return (suma);
 
 }
