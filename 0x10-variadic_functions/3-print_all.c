05886874c2c54ead9b6a5ecc7b1427ca207aa164
= <=>
2021-03-11 23:46:12 -0500

    Print Everything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,27 +3,47 @@
  * print_all - prints everything
  * @format: formato
  */
-void print_all(const char * const format, ...);
+void print_all(const char * const format, ...)
 {
+	int i = 0;
+	char *str;
+	va_list arg;
 
-va_list(ap);
-unsigned int i;
-char *str;
-
-va_start(ap, n);
-
-	for (i = 0; i < n; i++)
+	va_start(arg, format);
+	i = 0;
+	while (format == NULL)
 	{
-		str = va_arg(ap, char *)
-		if (str != NULL)
-			printf("%s", str);
-
-		else
-			printf("(nil)");
-
-		if (i != (n - 1) && separator != NULL)
-			printf("%s", separator)
+		printf("\n");
+		return;
 	}
-va_end(ap);
-printf("\n");
+	while (format[i] != '\0')
+	{
+		switch (format[i])
+		{
+		case 'c':
+			printf("%c", (char) va_arg(arg, int));
+			break;
+		case 'i':
+			printf("%d", va_arg(arg, int));
+			break;
+		case 'f':
+			printf("%f", (float)va_arg(arg, double));
+			break;
+		case 's':
+			str = va_arg(arg, char *);
+			if (str != NULL)
+			{
+				printf("%s", str);
+				break;
+			}
+			printf("(nil)");
+			break;
+		}
+		if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f'
+			|| format[i] == 's') &&  format[(i + 1)] != '\0')
+			printf(", ");
+		i++;
+	}
+	printf("\n");
+	va_end(arg);
 }
