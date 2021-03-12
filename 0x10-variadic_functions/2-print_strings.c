8f596c7ca0990e284500b27847e5bc6df79c7d2a
= <=>
2021-03-11 22:17:50 -0500

    Print_strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -9,6 +9,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 unsigned int i = 0;
 char *ch;
+
 va_list(ap);
 
 va_start(ap, n);
@@ -19,7 +20,7 @@ va_start(ap, n);
 
 			if (ch == NULL)
 			{
-				printf("(nil)");
+				printf("nil");
 			}
 
 		printf("%s", ch);
