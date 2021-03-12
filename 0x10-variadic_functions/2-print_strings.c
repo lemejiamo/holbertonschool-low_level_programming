2e35af0390b8111d35843fb2e6f35bbb3a513d2f
= <=>
2021-03-11 22:06:36 -0500

    Print_strings

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,35 @@
+#include "variadic_functions.h"
+/**
+ * print_strings - print strings
+ * @separator: separator
+ * @n: number of args to print
+ * Return: none
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+unsigned int i = 0;
+char *ch;
+va_list(ap);
+
+va_start(ap, n);
+
+                for (; i < n; i++)
+                {
+		ch = va_arg(ap, char *);
+
+			if(ch == NULL)
+			{
+				printf("(nil)");
+			}
+
+		printf("%s", ch);
+
+			if (separator != NULL && i != (n - 1))
+			{
+				printf("%s", separator);
+			}
+                }
+va_end(ap);
+printf("\n");
+
+}
