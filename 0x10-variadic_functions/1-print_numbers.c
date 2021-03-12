a8670200258082a5c513e1b1de9678539f7801f1
= <=>
2021-03-11 20:44:18 -0500

    Print_numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,40 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - print numbers with separator
+ * @separator: separator ", "
+ * @n: number of args
+ * Returns: none
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+unsigned int i = 0;
+
+va_list (ap);
+
+va_start (ap, n);
+
+	if(separator == NULL)
+	{
+
+		for(; i < n; i++ )
+		{
+			if (i == (n - 1))
+				printf("%d\n", va_arg(ap, unsigned int));
+			else
+				printf("%d", va_arg(ap, unsigned int));
+		}
+
+	}
+	else
+	{
+		for(; i < n; i++ )
+		{
+			if (i == (n - 1))
+				printf("%d\n", va_arg(ap, unsigned int));
+			else	
+				printf("%d%s", va_arg(ap, unsigned int), separator);
+		}
+	}
+va_end(ap);
+
+}
