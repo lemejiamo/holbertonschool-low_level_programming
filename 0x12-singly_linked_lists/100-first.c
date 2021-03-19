2cab2ebdbb7a1200f8940fa386b207860650a76b
= <=>
2021-03-19 17:08:14 -0500

    The fisrt out of main funtion

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,20 +1,12 @@
 #include <stdio.h>
-/*  tortoise() is executed before main() */
+/**
+ * tortoise - constructor before main
+ * Return: void
+ */
 void tortoise (void) __attribute__ ((constructor));
-
-
-/* libre() is executed after main() */
-void libre (void) __attribute__ ((destructor));
-
-
 
 void tortoise(void)
 {
 	printf ("You're beat! and yet, you must allow,\n");
-}
-
-
-void libre(void)
-{
 	printf ("I bore my house upon my back!\n");
 }
