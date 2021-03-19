497547303bc927d1946964774ce578f384311a3a
= <=>
2021-03-19 00:06:09 -0500

    Count the nodes of list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+* list_len - counting of nodes in a linked list
+* @h: pointer to head of liked
+* Return: the count of nodes
+*/
+size_t list_len(const list_t *h)
+{
+	if (h == NULL)
+        	return (0);
+
+return (1 + list_len(h->next));
+}
