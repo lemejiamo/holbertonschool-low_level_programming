68e868c580056d373d32b7aa1437c46c51c3ce5d
= <=>
2021-03-19 17:09:33 -0500

    The fisrt out of main funtion

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,10 +3,10 @@
  * tortoise - constructor before main
  * Return: void
  */
-void tortoise (void) __attribute__ ((constructor));
+void tortoise(void) __attribute__ ((constructor));
 
 void tortoise(void)
 {
-	printf ("You're beat! and yet, you must allow,\n");
-	printf ("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
