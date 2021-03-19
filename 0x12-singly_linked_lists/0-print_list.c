07b170e3a6338507383b8b24270c46e2bb5d4ca7
= <=>
2021-03-19 00:05:21 -0500

    Print a linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+* print_list - write in display the nodes
+* @h: poniter to head of linked list
+* Return: number of nodes
+*/
+size_t print_list(const list_t *h)
+{
+	if (h == NULL)
+		return (0);
+if (!h->str)		
+printf("[%d] %s\n", h->len, "(nil)");
+else
+printf("[%d] %s\n", h->len, h->str);
+return (1 + print_list(h->next));
+}
