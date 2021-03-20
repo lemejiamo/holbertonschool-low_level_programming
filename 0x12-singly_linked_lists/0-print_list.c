9c298b8269de0b329c5fbb94a0933916c0dedbe3
= <=>
2021-03-20 14:15:38 -0500

    correction of betty style

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
 * print_list - write in display the nodes
 * @h: poniter to head of linked list
@@ -9,9 +8,12 @@ size_t print_list(const list_t *h)
 {
 	if (h == NULL)
 		return (0);
+
 	if (!h->str) /* h->str == NULL */
 		printf("[%d] %s\n", h->len, "(nil)");
+
 	else
 		printf("[%d] %s\n", h->len, h->str);
+
 return (1 + print_list(h->next));
 }
