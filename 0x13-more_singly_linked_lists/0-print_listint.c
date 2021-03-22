e85a91dd6091a22c5bb9d07feea55cad5b9fa5e7
= <=>
2021-03-22 15:18:45 -0500

    print the  value of a element in a node

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ *print_listint - prints the number of nodes in a list
+ *@h: pointer to list to count
+ *Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	if (h == NULL)
+		return (0);
+
+	printf("%d\n", h->n);
+
+	return (1 +(print_listint(h->next)));
+}
