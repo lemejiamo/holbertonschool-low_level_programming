cc5ae63de2207f85cb88029a4918964491715557
= <=>
2021-03-22 15:46:44 -0500

    Count number of nodes from a list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+/**
+ *listint_len - count  the number of nodes in a list
+ *@h: pointer to list to head of list
+ *Return: number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	if (h == NULL)
+		return (0);
+
+	return (1 +(listint_len(h->next)));
+}
