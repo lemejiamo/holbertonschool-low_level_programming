78c1ae681da6ca2a42908018806e13f6d4062756
= <=>
2021-03-23 12:25:20 -0500

    insert node in a specific position

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ *insert_nodeint_at_index - insert node in specific position
+ *@head: pointer to´pointer head
+ *@idx: index
+ *@n: data to new node
+ *Return: address of newnode
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+
+	listint_t *newnode = NULL, *temp = NULL;
+
+	newnode = malloc(sizeof(listint_t));
+	if (newnode == NULL)
+		return (NULL);
+
+	newnode->n = n;
+	temp = *head;
+	for (; idx > 1; idx--)
+	{
+		temp = (temp)->next;
+	}
+	newnode->next = (temp)->next;
+	(temp)->next = newnode;
+	return (newnode);
+
+}
