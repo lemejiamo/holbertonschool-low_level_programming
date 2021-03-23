e464a2e393397bb324e8b0b0b40f25f3483b380f
= <=>
2021-03-23 06:36:01 -0500

    betty style correction

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,13 +7,12 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
-	if(*head == NULL)
+
+	if (*head == NULL)
 		return;
 	temp = *head;
 	temp = temp->next;
-
 	free_listint2(&temp);
-
 	free(*head);
 	*head = NULL;
 }
