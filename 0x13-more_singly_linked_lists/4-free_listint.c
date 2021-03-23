5269fcec81c95652a88dddd8f499b879148b82fa
= <=>
2021-03-23 01:50:33 -0500

    free list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+/**
+ *free_listint - free all malloc allocation
+ *@head: pointer to head of the list
+ *Return: none
+ */
+void free_listint(listint_t *head)
+{
+	if (head == NULL)
+		return;
+	free_listint(head->next);
+	free (head);
+}
