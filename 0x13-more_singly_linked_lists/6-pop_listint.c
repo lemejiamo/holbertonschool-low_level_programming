f8a75378d0bdc1ee87f61d16cbbb981f6224bc23
= <=>
2021-03-23 04:15:06 -0500

    delete head of a node

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * pop_listint - deletes a head of list
+ * @head: pointer to head of list
+ * Return: 0 succes -1 error
+ */
+int pop_listint(listint_t **head)
+{
+	int data;
+	listint_t *temp;
+
+	temp = *head;
+	data = temp->n;
+	*head = (*temp).next; /* asignamos la nueva cabeza */
+	free(temp);
+	return (data);
+}
