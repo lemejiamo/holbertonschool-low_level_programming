514ea09f600ce74030d57154aa8347ab488e958f
= <=>
2021-03-23 12:41:48 -0500

    insert node in a specific position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,7 +10,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 
 	listint_t *newnode = NULL, *temp = NULL;
-	unsigned int i = 1;
+	
 
 	newnode = malloc(sizeof(listint_t));
 	if (newnode == NULL)
@@ -18,9 +18,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	newnode->n = n;
 	temp = *head;
-	for (; i < idx; i++, temp = (temp)->next)
-	{ }
-	newnode->next = (temp)->next;
+	for (; idx > 1; idx--)
+	{
+		temp = (temp)->next;
+        }
+	newnode->next = temp->next;
 	(temp)->next = newnode;
 	return (newnode);
 
