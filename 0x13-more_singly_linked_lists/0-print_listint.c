75b7d3981549c9e79cd21ab68a6198e2af022ca6
= <=>
2021-03-23 06:38:08 -0500

    print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,8 +8,10 @@ size_t print_listint(const listint_t *h)
 {
 	if (h == NULL)
 		return (0);
-
-	printf("%d\n", h->n);
+/*	if (!h->n)*/
+/*		printf("(nill)\n"); */
+/*	else */
+		printf("%d\n", h->n);
 
 	return (1 +(print_listint(h->next)));
 }
