4477e96381bcc496164b32a779b2d972cf7c9290
= <=>
2021-03-23 06:30:22 -0500

    get data of index node

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_end - get the node data from  specific position
+ * @head: poniter to head of a list
+ * @index: the position to read
+ * Return: data from index
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int counter = 0;
+	listint_t *temp;
+
+	temp = head;
+
+	for (; counter != index; counter++)
+	{
+		temp = temp->next;
+	}
+	return(temp);
+}
