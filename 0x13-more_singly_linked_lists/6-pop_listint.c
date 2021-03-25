88e3e86ea6143ea350328d9196dca2b039edad69
= <=>
2021-03-25 10:25:19 -0500

    correction of edge case

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -10,7 +10,7 @@ int pop_listint(listint_t **head)
 	listint_t *temp;
 
 	if (head == NULL || *head == NULL)
-                return;
+		return;
 
 	temp = *head;
 	data = temp->n;
--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -14,6 +14,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	for (; counter != index; counter++)
 	{
+		if (temp->next == NULL)
+			return (NULL);
 		temp = temp->next;
 	}
 	return (temp);
