90135d7604333cd585e3ab1ac2bf072fcb721ff2
= <=>
2021-03-25 10:51:52 -0500

    correction of edge case index = 1

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -3,7 +3,7 @@
 * delete_nodeint_at_index - DELETE A NODE
 * @head: pointer to pointer a head of a list
 * @index: position to delete
-* Return: 1 if success -1 if fail
+* Return: 1 if successe -1 if fail
 */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
@@ -27,13 +27,12 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		if (temp == NULL)
 			return (-1);
-		if (i + 1 == index - 1)
+		if (i  == index - 1)
 			before = temp->next;
 		temp = temp->next;
 	}
 
 	before->next = temp->next;
-
 	free(temp);
 	return (1);
 }
