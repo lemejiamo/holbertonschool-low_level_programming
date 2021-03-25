25c763a33588734cb351662950f1a6b2928c718c
= <=>
2021-03-25 09:48:24 -0500

    correction of edge case

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,7 +7,8 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
-	if (head == NULL || *head ==NULL)
+
+	if (head == NULL || *head == NULL)
 		return;
 
 	for (temp = *head; temp != NULL; *head = temp)
