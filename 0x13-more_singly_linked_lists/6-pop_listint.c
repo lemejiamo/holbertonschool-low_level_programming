a2fc7f7c10106fa561175fa8b97e545ac620faec
= <=>
2021-03-25 10:23:46 -0500

    correction of edge case

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,6 +9,9 @@ int pop_listint(listint_t **head)
 	int data;
 	listint_t *temp;
 
+	if (head == NULL || *head == NULL)
+                return;
+
 	temp = *head;
 	data = temp->n;
 	*head = (*temp).next; /* asignamos la nueva cabeza */
