a9740b3451eeef3c5bb76b85093972312bec27db
= <=>
2021-03-28 21:06:02 -0500

    correction of code

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -4,19 +4,17 @@
  * main -  seguimos contando
  * Return: 0
 */
-
 int main(void)
-
 {
-	int n;
+int n;
 		for (n = 48; n <= 57; n++)
 		{
 			putchar(n);
-		{
+		}
 		for (n = 97; n <= 102; n++)
 		{
 			putchar(n);
 		}
 	putchar('\n');
-	return (0);
+return (0);
 }
