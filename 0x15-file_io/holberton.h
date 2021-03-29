e07c81b3c5f5d464ae0b32d503e45617a82049a7
= <=>
2021-03-29 17:10:07 -0500

    HEADER FILE

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -2,6 +2,9 @@
 #define HOLBERTON_H
 #include <unistd.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
