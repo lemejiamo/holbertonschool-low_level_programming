45b17515e2e96cdd8ca4e4aa7a619e98c4629674
= <=>
2021-03-29 17:05:35 -0500

    learnig to use syscalls read, open,  Write

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ *read_textfile - read text in a file and prints in stdout
+ *@filename: file to read
+ *@letters: count of letter to print
+ *Return: chars printed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+	int readed  = 0, fd;
+	char  buffer[1024];
+
+	if (filename == NULL)
+	{
+		printf("No file\n");
+		return (0);
+	}
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	readed = read(fd, buffer, letters);
+	if (readed == -1)
+		return (0);
+
+	write(1, buffer, readed);
+	return (readed);
+}
