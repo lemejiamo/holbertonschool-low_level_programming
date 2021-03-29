ab527801f11eefb5eb70c7af0d6b1f41c5261094
= <=>
2021-03-29 17:39:46 -0500

    learnig to use syscalls read, open,  Write

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,14 +14,17 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
+       	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 
 	readed = read(fd, buffer, letters);
 	if (readed == -1)
+	{
+		close(fd);
 		return (0);
-
+	}
 	write(1, buffer, readed);
+	close(fd);
 	return (readed);
 }
