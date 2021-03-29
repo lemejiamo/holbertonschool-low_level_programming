d23ed344fc1149d848e632ee3d69e8951d5881d5
= <=>
2021-03-28 20:55:33 -0500

    correction Betty style

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,19 +1,19 @@
 # include <stdio.h>
 # include <stdlib.h>
-/*
+/**
  * main - el alfabeto en minuscula
  * Return: siempre 0
 */
 
-int main (void)
+int main(void)
 
 {
 	int n;
-		for (n=97; n <= 122; n++) 
-			if ( (n != 101) && (n != 113 ) ) 
-				{
-					putchar (n);
-				}
+		for (n = 97; n <= 122; n++)
+			if ((n != 101) && (n != 113))
+			{
+				putchar (n);
+			}
 		putchar('\n');
 	return (0);
 }
