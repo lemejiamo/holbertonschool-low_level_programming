edce7d8ec6189ead8e3ea14bf30a2943f4e21d2b
lemejiamo <77980644+lemejiamo@users.noreply.github.com>
2021-04-12 23:50:45 -0500

    Add files via upload

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_triangle - prints a triangle
+ * @size : size of the triangle
+ *
+ * Return : None
+ */
+void print_triangle(int size)
+{
+	int fila, sim, esp;
+
+	if (size <= 0)
+		_putchar('\n');
+	
+	for (fila = 1; fila <= size; fila++)
+	{
+		for (esp = size - fila; esp > 0; esp--)
+			_putchar(' ');
+		for (sim = fila; sim > 0; sim--)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
