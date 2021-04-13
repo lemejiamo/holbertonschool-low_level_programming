d90f7a40f2d322a1fc55bea5087d2aad8d5a7c4c
lemejiamo <77980644+lemejiamo@users.noreply.github.com>
2021-04-13 00:08:17 -0500

    Add files via upload

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,40 +1,16 @@
-        .string "Hello, Holberton"
-        .text
-        .globl  main
-        .type   main, @function
-main:
-.LFB2:
-        .cfi_startproc
-        pushq   %rbp
-        .cfi_def_cfa_offset 16
-        .cfi_offset 6, -16
-        movq    %rsp, %rbp
-        .cfi_def_cfa_register 6
-        subq    $16, %rsp
-        movq    $.LC0, -8(%rbp)
-        movl    -12(%rbp), %eax
-        movl    %eax, %edi
-        movl    $0, %eax
-        call    open
-        movl    %eax, -12(%rbp)
-        movq    -8(%rbp), %rax
-        movq    %rax, %rdi
-        call    strlen
-        movq    %rax, %rdx
-        movq    -8(%rbp), %rax
-        movzbl  (%rax), %eax
-        movsbl  %al, %ecx
-        movl    -12(%rbp), %eax
-        movl    %ecx, %esi
-        movl    %eax, %edi
-        movl    $0, %eax
-        call    write
-        movl    -12(%rbp), %eax
-        movl    %eax, %edi
-        movl    $0, %eax
-        call    close
-        leave
-        .cfi_def_cfa 7, 8
-        ret
-        .cfi_endproc
+section .data
+	global main
 
+main:
+	mov rax, 4
+	mov rbx, 1
+	mov rcx, message
+	mov rdx, length
+	int 80h
+
+	mov rax, 1
+	mov rbx, 0
+	int 80h
+
+message:		db "Hello, Holberton", 10
+length:		    equ $ - holbiemessage
