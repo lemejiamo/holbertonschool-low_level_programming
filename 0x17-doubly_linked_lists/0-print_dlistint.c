7a5bd89032523b97f6761dcfdc3a3b1ba8f57a4c
lemejiamo <luismejia69@gmail.com>
2021-05-01 20:35:50 -0400

    print list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -13,11 +13,11 @@ size_t print_dlistint(const dlistint_t *h)
 
 	if (h == NULL)
 		return (0);
+	/* loop to print the nodes */
 	for (i = 0; h != NULL; i++)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
 	}
-
-return (i);
+	return (i);
 }
