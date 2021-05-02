7633662500b9ba0f7538bb01a819da53b9f655ad
lemejiamo <luismejia69@gmail.com>
2021-05-02 01:25:39 -0400

    free list

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,13 +1,11 @@
 #include "lists.h"
 /**
- * add_dnodeint_end - add node at end of a list
+ * free_dlistint - free list
  * @head: pointer to head of list
- * @n: value in int node
- * Return: number of nodes printed
  */
 void free_dlistint(dlistint_t *head)
 {
-    dlistint_t *prev = NULL;
+	dlistint_t *prev = NULL;
 	/*memory assing */
 	if (!head)
 		exit(1);
@@ -17,12 +15,12 @@ void free_dlistint(dlistint_t *head)
 		head = head->next;
 	}
 
-    do{ 
-    prev = head->prev;
-    free(head);
-    head = prev;
-    prev = prev->prev;
-    }while (prev->prev != NULL);
-    free(head);
-    free(prev);
+	do {
+		prev = head->prev;
+		free(head);
+		head = prev;
+		prev = prev->prev;
+	} while (prev->prev != NULL);
+	free(head);
+	free(prev);
 }
