70229433b349ee63182035954a1cf4558be425b5
lemejiamo <luismejia69@gmail.com>
2021-05-03 00:27:17 -0400

    print elements from linked list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -6,15 +6,8 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-  int i = 0;
-  
-  if (h == NULL)
-    return (0);
-
-  for (i = 0; h != NULL; i++)
-  {
-    printf("%d\n", h->n);
-    h = h->next;
-  }
-  return (i);
+	if (!h)
+		return (0);
+	printf("%d\n", h->n);
+	return (1 + print_dlistint(h->next));
 }
