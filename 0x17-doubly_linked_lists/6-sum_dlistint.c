94d72a742c9175e20b3e3b04759db41fdc38168e
lemejiamo <luismejia69@gmail.com>
2021-05-02 21:49:56 -0400

    add elements in a linked list

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+* sum_dlistint - sum elements in a list
+* @head: pointer to head of a list
+* Return: add of elements
+*/
+int sum_dlistint(dlistint_t *head)
+{
+	/* inicializacion de variables */
+	int add = 0;
+	dlistint_t *current = NULL;
+
+	current = head;
+	do {
+		add += current->n;
+		current = current->next;
+	} while (current->next != NULL);
+	add += current->n;
+	return (add);
+}
