3a7b8fa5e163eb3a6b0879a079dec3854195beaf
lemejiamo <luismejia69@gmail.com>
2021-05-28 02:51:40 -0500

    print a hash_table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - print hash table.
+ * @ht: pointer to hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned int i = 0, begin = 0;
+	hash_node_t *slot = NULL;
+
+	printf("{");
+
+	for (; i <  ht->size; i++)
+	{
+		slot = ht->array[i];
+		while (slot != NULL)
+		{
+			if(begin != 0)
+				printf(", ");
+			printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+			slot = slot->next;
+			begin++;
+		}
+	}
+	printf("}\n");
+}
