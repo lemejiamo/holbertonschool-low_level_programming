69bb0fdafbc2ab659afa332754ec84ace065dafd
lemejiamo <luismejia69@gmail.com>
2021-05-28 01:17:39 -0500

    create a hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - create a hasta table of specific size
+ * @size: size of table
+ * Return: a pointer to head of hash_table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	/* declaration of pointer to a table */
+	hash_table_t *table_hash = NULL;
+
+	table_hash = malloc(sizeof(hash_table_t));
+	if (table_hash == NULL)
+	{
+		return (NULL);
+	}
+
+	table_hash->size = size;
+	table_hash->array = malloc(sizeof(hash_node_t) * size);
+	if (table_hash->array == NULL)
+	{
+		return (NULL);
+	}
+
+	while ((int)size >= 0)
+	{
+
+		table_hash->array[size] = NULL;
+	/*	printf("%p position --> %d\n", (void *)table_hash->array[1025], (int)size); */
+		size--;
+	}
+	return (table_hash);
+}
