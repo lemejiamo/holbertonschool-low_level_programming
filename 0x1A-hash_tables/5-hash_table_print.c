44fd450ec21b11c4645c234727329c7e7e9501ef
lemejiamo <luismejia69@gmail.com>
2021-05-28 12:48:48 -0500

    add validation line 11

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -8,7 +8,7 @@ void hash_table_print(const hash_table_t *ht)
 	unsigned int i = 0, begin = 0;
 	hash_node_t *slot = NULL;
 
-	if (ht == NULL || ht->array == NULL) 
+	if (ht == NULL || *ht->array == NULL) 
 		return;
 
 	printf("{");
