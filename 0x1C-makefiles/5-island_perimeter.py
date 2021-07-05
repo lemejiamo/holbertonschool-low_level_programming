26568beb9b65d32bfe127df09fad2b5517212eed
lemejiamo <luismejia69@gmail.com>
2021-07-04 19:33:30 -0500

    island perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+"""  programm to calcute the perimeter of island """
+
+
+def island_perimeter(grid):
+    """
+        calculate the perimeter based in a grid
+
+        Attributes:
+        Grid: is rectangular, width and height don’t exceed 100
+    """
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                potential_perimeter = 4
+                if grid[i][j+1] == 1:
+                    potential_perimeter -= 1
+                if grid[i][j-1] == 1:
+                    potential_perimeter -= 1
+                if grid[i-1][j] == 1:
+                    potential_perimeter -= 1
+                if grid[i+1][j] == 1:
+                    potential_perimeter -= 1
+                perimeter = perimeter + potential_perimeter
+    return (perimeter)
